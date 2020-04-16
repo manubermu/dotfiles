c1539d274e6374bc8eaa84e47eb8ea7afda0ed22
Manuel Bermúdez Martínez <manubermu@Mini-de-Manuel.lan>
2020-04-16 22:06:45 +0200

    [MOD] mods

--- install.sh
+++ install.sh
@@ -11,26 +11,6 @@ export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ###############################################################################
-# Set permissions                                                             #
-###############################################################################
-
-for INSTALLFILE in "$DOTFILES_DIR"/install/*.sh; do
-    chmod +x INSTALLFILE
-done
-
-for INSTALLFILE in "$DOTFILES_DIR"/config/*.sh; do
-    chmod +x INSTALLFILE
-done
-
-for INSTALLFILE in "$DOTFILES_DIR"/config/duti/*.sh; do
-    chmod +x INSTALLFILE
-done
-
-for INSTALLFILE in "$DOTFILES_DIR"/config/zsh/*.sh; do
-    chmod +x INSTALLFILE
-done
-
-###############################################################################
 # Menu                                                                        #
 ###############################################################################
 
@@ -47,7 +27,6 @@ do
     #  (3) Configure macOS                                                        #
     #  (4) Configure extensions                                                   #
     #  (5) Install/Configure zsh                                                  #
-    #  (6) Update                                                                 #
     #  (0) Exit                                                                   #
     #                                                                             #
     ###############################################################################
@@ -59,7 +38,6 @@ EOF
     "3")  echo "Ready to configure macOS"                | $DOTFILES_DIR/config/macos.sh ;;
     "4")  echo "Ready to configure extensions"           | $DOTFILES_DIR/config/duti/set.sh ;;
     "5")  echo "Ready to install/configure zsh"          | $DOTFILES_DIR/zsh/zsh.sh ;;
-    "6")  echo "Ready to update..."                      | $DOTFILES_DIR/update.sh ;;
     "0")  exit                                                                   ;;
      * )  echo "Invalid option!"                                                 ;;
     esac
